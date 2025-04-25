b9db6e913d835e73753b9e1ec723b3c2a8da5728
Cliff-Chiama <Clifford.Chiama@Strathmore.Edu>
2025-04-25 09:06:33 +0300

    First Commit

---
+++ Leap Year.py
@@ -0,0 +1,14 @@
+
+year:int(input("Enter a Year:"))
+
+if (year%4) == 0:
+        if (year%100) ==0:
+            if(year%400)==0:
+                leap_year = True
+
+            else:
+                leap_year = False
+        else:
+            leap_year = True
+else:
+        leap_year = False
